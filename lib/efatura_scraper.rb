5775a0a8ffcb4bf9645113d98d6f17713f140e48
filipe custodio <filipe.l.custodio@gmail.com>
2019-01-04 23:18:18 +0000

    generated gem skeleton

---
+++ lib/efatura_scraper.rb
@@ -0,0 +1,6 @@
+require "efatura_scraper/version"
+
+module EfaturaScraper
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/efatura_scraper/version.rb
@@ -0,0 +1,3 @@
+module EfaturaScraper
+  VERSION = "0.1.0"
+end
