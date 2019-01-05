8ad7707fd2a73429a38b20a5dcba906b9ba2bfdc
filipe custodio <filipe.l.custodio@gmail.com>
2019-01-05 15:36:28 +0000

    added cookies to configuration file. Starts with an empty hash

--- lib/efatura/configuration.rb
+++ lib/efatura/configuration.rb
@@ -1,11 +1,12 @@
 module Efatura
   class Configuration
-    attr_accessor :nif, :password
+    attr_accessor :nif, :password, :cookies
     # SET THE CONFIGURATION INITIALIZER WITH NIL VALUES SINCE THIS INFORMATION
     # WILL COME FROM THE GEM USER
     def initialize
       @nif = nil
       @password = nil
+      @cookies = {}
     end
   end
 end
