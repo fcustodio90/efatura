08b8a5487ec232dc8301bc95db2a0be6badd1500
filipe custodio <filipe.l.custodio@gmail.com>
2019-01-05 18:50:27 +0000

    major refactor across all the code. Calling the methods now works but date validation is still being hardcoded

--- lib/efatura.rb
+++ lib/efatura.rb
@@ -15,10 +15,6 @@ module Efatura
   CONSUMIDOR_URL = 'https://faturas.portaldasfinancas.gov.pt/painelAdquirente.action'
   FATURAS_URL = 'https://faturas.portaldasfinancas.gov.pt/json/obterDocumentosAdquirente.action'
 
-  class << self
-    attr_accessor :configuration
-  end
-
   def self.configuration
     @configuration ||= Configuration.new
   end
@@ -31,75 +27,86 @@ module Efatura
     yield(configuration)
   end
 
-  def invoices
-    # SET THE RESPONSE REQUEST BY GIVING THE AJAX / JSON URL USED BY EFATURA
-    # AND ASSIGNING THE COOKIE HEADERS
-    response = RestClient::Request.execute(
-      method: :get,
-      url: FATURAS_URL,
-      # CALL THE LOGIN METHOD THAT RETURNS AN HASH OF COOKIES
-      cookies: login,
-      headers: {
-        params: {
-          'dataInicioFilter' => s_date,
-          'dataFimFilter' => e_date,
-          'ambitoAquisicaoFilter' => 'TODOS'
+  def self.invoices(from_date, to_date)
+    Client.new(from_date, to_date).invoices
+  end
+
+  class Client
+    attr_accessor :from_date, :to_date
+
+    def initialize(from_date, to_date)
+      @from_date = from_date
+      @to_date = to_date
+    end
+
+    def invoices
+      # SET THE RESPONSE REQUEST BY GIVING THE AJAX / JSON URL USED BY EFATURA
+      # AND ASSIGNING THE COOKIE HEADERS
+      response = RestClient::Request.execute(
+        method: :get,
+        url: FATURAS_URL,
+        # CALL THE LOGIN METHOD THAT RETURNS AN HASH OF COOKIES
+        cookies: login,
+        headers: {
+          params: {
+            'dataInicioFilter' => '2017-01-01',
+            'dataFimFilter' => '2017-02-01',
+            'ambitoAquisicaoFilter' => 'TODOS'
+          }
         }
-      }
-    )
-    # RETURNS ALL THE INVOICES REGISTED IN EFATURA AT THE GIVEN TIMEFRAME
-    JSON.parse(response)
-  end
+      )
+      # RETURNS ALL THE INVOICES REGISTED IN EFATURA AT THE GIVEN TIMEFRAME
+      JSON.parse(response)
+    end
 
-  private
+    private
 
-  def date_valid?
-    # FOR A DATE TO BE VALID IT NEEDS TO PASS THE THREE CONDITIONS
-    date_format_valid?(s_date) && date_format_valid?(e_date) && date_same_year?
-  end
+    def date_valid?(to_date, from_date)
+      # FOR A DATE TO BE VALID IT NEEDS TO PASS THE THREE CONDITIONS
+      date_format_valid?(from_date) && date_format_valid?(to_date) && date_same_year?
+    end
 
-  def date_format_valid?(date)
-    # RECEIVE A DATE INSTANCE VARIABLE AND VERIFIES THE CORRECT FORMAT
-    format = '%Y-%m-%d'
-    DateTime.strptime(date, format)
-    true
-  rescue ArgumentError
-    false
-  end
+    def date_format_valid?(date)
+      # RECEIVE A DATE INSTANCE VARIABLE AND VERIFIES THE CORRECT FORMAT
+      format = '%Y-%m-%d'
+      DateTime.strptime(date, format)
+      true
+    rescue ArgumentError
+      false
+    end
 
-  def date_same_year?
-    # ANALYZES IF BOTH DATES ARE FROM THE SAME YEAR
-    s_date[0..3] == e_date[0..3]
-  end
+    def date_same_year?(from_date, to_date)
+      Date.parse(from_date).year == Date.parse(to_date).year
+    end
 
-  def login
-    # IF DATE IS VALID EXECUTES THE LOGIN METHOD
-    if date_valid?
-      # SET AN EMPTY HASH TO FEED LATER WITH AGENT COOKIE JAR
-      cookies = {}
-      # INITIATE A NEW MECHANIZE INSTANCE
-      agent = Mechanize.new
-      # FETCH THE LOGIN URL AND ITERATES THRO LOGIN_PAGE IN ORDER TO TARGET HTML COMPONENTS
-      agent.get(LOGIN_URL) do |login_page|
-        # FETCHES THE LOGIN FORM FROM THE LOGIN PAGE
-        login_form = login_page.form_with(name: 'loginForm')
-        # SETS THE LOGIN USERNAME AKA NIF / NUMERO CONTRIBUINTE
-        login_form.username = nif
-        # SETS THE LOGIN PASSWORD
-        login_form.password = password
-        # SUBMITS THE FORM / LOGIN
-        agent.submit(login_form)
-        # AFTER A SUCCESSFUL LOGIN FETCH THE CONSUMIDOR PAGE IN ORDER TO RETRIEVE COOKIES
-        consumidor_page = agent.get(CONSUMIDOR_URL)
-        # ASSIGN THE CONSUMIDOR FORM TO A VARIABLE
-        consumidor_form = consumidor_page.form_with(name: 'form')
-        # SUBMIT THE FORM
-        agent.submit(consumidor_form)
-        # MAPS THE COOKIE_JAR FROM AGENT OBJECT AND FEEDS IT TO THE COOKIES HASH
-        # THAT WAS INITIALIZED WITH AN EMPTY HASH
-        cookies = Hash[agent.cookie_jar.store.map { |i| i.cookie_value.split('=') }]
-        cookies
-      end
+    def login
+      # IF DATE IS VALID EXECUTES THE LOGIN METHOD
+      #if date_valid?
+        # SET AN EMPTY HASH TO FEED LATER WITH AGENT COOKIE JAR
+        cookies = {}
+        # INITIATE A NEW MECHANIZE INSTANCE
+        agent = Mechanize.new
+        # FETCH THE LOGIN URL AND ITERATES THRO LOGIN_PAGE IN ORDER TO TARGET HTML COMPONENTS
+        agent.get(LOGIN_URL) do |login_page|
+          # FETCHES THE LOGIN FORM FROM THE LOGIN PAGE
+          login_form = login_page.form_with(name: 'loginForm')
+          # SETS THE LOGIN USERNAME AKA NIF / NUMERO CONTRIBUINTE
+          login_form.username = Efatura.configuration.nif
+          # SETS THE LOGIN PASSWORD
+          login_form.password = Efatura.configuration.password
+          # SUBMITS THE FORM / LOGIN
+          agent.submit(login_form)
+          # AFTER A SUCCESSFUL LOGIN FETCH THE CONSUMIDOR PAGE IN ORDER TO RETRIEVE COOKIES
+          consumidor_page = agent.get(CONSUMIDOR_URL)
+          # ASSIGN THE CONSUMIDOR FORM TO A VARIABLE
+          consumidor_form = consumidor_page.form_with(name: 'form')
+          # SUBMIT THE FORM
+          agent.submit(consumidor_form)
+          # MAPS THE COOKIE_JAR FROM AGENT OBJECT AND FEEDS IT TO THE COOKIES HASH
+          # THAT WAS INITIALIZED WITH AN EMPTY HASH
+          return cookies = Hash[agent.cookie_jar.store.map { |i| i.cookie_value.split('=') }]
+        end
+      #end
     end
   end
 end
--- lib/efatura/configuration.rb
+++ lib/efatura/configuration.rb
@@ -1,6 +1,6 @@
 module Efatura
   class Configuration
-    attr_accessor :nif, :password, :cookies
+    attr_accessor :nif, :password
     # SET THE CONFIGURATION INITIALIZER WITH NIL VALUES SINCE THIS INFORMATION
     # WILL COME FROM THE GEM USER
     def initialize
